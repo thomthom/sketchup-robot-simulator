6d4c0f75c3146370722f5a6ee357c2f1afe9af69
Thomas Thomassen <thomas@thomthom.net>
2016-07-07 20:09:49 +0200

    adds controls to init and start simulation

--- src/tt_bot/core.rb
+++ src/tt_bot/core.rb
@@ -3,6 +3,14 @@ require 'tt_bot/simulation_tool'
 module TT::Plugins::Bot
 
   unless file_loaded?(__FILE__)
+    cmd = UI::Command.new('Initialize Simulation') {
+      self.init_simulation
+    }
+    cmd.tooltip = 'Initialize Bot Simulation'
+    cmd.small_icon = File.join(PATH, 'icons', 'iconmonstr-media-control-47.svg')
+    cmd.large_icon = File.join(PATH, 'icons', 'iconmonstr-media-control-47.svg')
+    cmd_init_simulation = cmd
+
     cmd = UI::Command.new('Start Simulation') {
       self.start_simulation
     }
@@ -12,15 +20,17 @@ module TT::Plugins::Bot
     cmd_start_simulation = cmd
 
     toolbar = UI.toolbar('Bot Simulation')
+    toolbar.add_item(cmd_init_simulation)
     toolbar.add_item(cmd_start_simulation)
 
-    menu = UI.menu('Plugins')
+    menu = UI.menu('Plugins').add_submenu('Bot Simulation')
+    menu.add_item(cmd_init_simulation)
     menu.add_item(cmd_start_simulation)
 
     file_loaded(__FILE__)
   end
 
-  def self.start_simulation
+  def self.init_simulation
     model = Sketchup.active_model
     path = model.entities.grep(Sketchup::Edge).first
     arrow = model.entities.grep(Sketchup::ConstructionPoint).first
@@ -29,6 +39,12 @@ module TT::Plugins::Bot
 
     @tool = SimulationTool.new(path, arrow)
     model.select_tool(@tool)
+    nil
+  end
+
+  def self.start_simulation
+    self.init_simulation if @simulation_tool.nil?
+    @tool.simulation.start
   end
 
   def self.simulation
